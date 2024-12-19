1badaee3b46e3c78ff6c66f413a489432d056059
DimasBratakusumah <dimas.bratakusuma@gmail.com>
2024-12-19 20:10:19 +0700

    [Navigate] move the direction of the destination

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/sensor_dashboard.dart';
-import 'screens/control_dashboard.dart';
+import 'package:iop_app/screens/home_dashboard.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,50 +14,50 @@ class MyApp extends StatelessWidget {
       title: 'IoT App',
       theme: ThemeData(primarySwatch: Colors.blue),
       debugShowCheckedModeBanner: false,
-      home: const MainScreen(),
+      home: HomePage(),
     );
   }
 }
 
-class MainScreen extends StatefulWidget {
-  const MainScreen({Key? key}) : super(key: key);
+// class MainScreen extends StatefulWidget {
+//   const MainScreen({Key? key}) : super(key: key);
 
-  @override
-  State<MainScreen> createState() => _MainScreenState();
-}
+//   @override
+//   State<MainScreen> createState() => _MainScreenState();
+// }
 
-class _MainScreenState extends State<MainScreen> {
-  int _selectedIndex = 0;
+// class _MainScreenState extends State<MainScreen> {
+//   int _selectedIndex = 0;
 
-  static final List<Widget> _pages = <Widget>[
-    SensorDashboard(),
-    ControlDashboard(),
-  ];
+//   static final List<Widget> _pages = <Widget>[
+//     SensorDashboard(),
+//     ControlDashboard(),
+//   ];
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.monitor),
-            label: 'Monitoring',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.control_camera),
-            label: 'Controlling',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: _pages[_selectedIndex],
+//       bottomNavigationBar: BottomNavigationBar(
+//         items: const <BottomNavigationBarItem>[
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.monitor),
+//             label: 'Monitoring',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.control_camera),
+//             label: 'Controlling',
+//           ),
+//         ],
+//         currentIndex: _selectedIndex,
+//         onTap: _onItemTapped,
+//       ),
+//     );
+//   }
+// }
