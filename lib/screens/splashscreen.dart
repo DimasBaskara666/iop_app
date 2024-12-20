1e89257cc5ca606767290bbc12c4b8e2d960ba26
DimasBratakusumah <dimas.bratakusuma@gmail.com>
2024-12-20 13:14:41 +0700

    [UI] Adding splashscreen

---
+++ lib/screens/splashscreen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import '../screens/home_dashboard.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 5), () {
+      // Navigate to your next screen (e.g., HomeScreen).
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'SMART',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 32),
+              decoration: BoxDecoration(
+                color: Color(0xFFFFC99B),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Text(
+                'CASA',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
